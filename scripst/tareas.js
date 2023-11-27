4824d390ed4a5e7bca4134c782c477ea1b8d1f42
Torremolinos <adrian_2054@hotmail.com>
2023-11-27 08:50:58 +0100

    agregamos la clase Tareas

---
+++ scripst/tareas.js
@@ -0,0 +1,15 @@
+/** 
+ *Author:Adrián Iglesias Riño
+  Github:https://github.com/Torremolinos/Poo-Proyect*/
+
+class Tarea {
+    constructor(id, descripcion) {
+        this.id = id;
+        this.descripcion = descripcion;
+    }
+    
+    editar(descripcion){
+        this.descripcion = descripcion;
+
+    }
+}
